23aefcaf4196a2701a985a3c417126ec6e57e151
Sanjayappu2304 <114397338+Sanjayappu2304@users.noreply.github.com>
2022-11-12 15:44:35 +0530

    Update app.py

--- Final Deliverables/flask app/app.py
+++ Final Deliverables/flask app/app.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def bot():
+    return render_template('Chatbot.html')
+   
+
+if __name__ == ' __main__':
+    app.run() 
